c2045fb86a4435b1bd5e2dee77a136bbcf13b4dc
skwogus98 <skwogus98@gmail.com>
2023-07-28 02:50:17 +0900

    add Contact page

--- src/pages/ContactPage.vue
+++ src/pages/ContactPage.vue
@@ -3,7 +3,11 @@
         <router-link class="h-12 absolute top-80 animate-bounce" to="/Skills"><img src="../img/left-arrow.png" class="h-12" alt="" /></router-link>
         <div class="w-screen h-screen py-24 px-12 flex">
             <div class="w-44 mx-8 text-4xl border-r-2 pr-6 border-gray-500">Contact</div>
-            
+            <div class="flex flex-col space-y-4">
+                <p>📧 zkxmwogus@naver.com</p>
+                <p><a href="http://www.github.com/skwogus98">👾 github.com/skwogus98</a></p>
+                <p>📱 010-5044-5388</p>
+            </div>
         </div>
     </div>
 </template>
